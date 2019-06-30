99e1bff7b741f684b139caa1cb9a3670cafa32c5
Asuka Wang <asukachikaru@gmail.com>
2019-06-30 18:12:09 +0900

    Style change with new dummy data (#7)
    * Photo list view
    
    * Photo list view with dummy data
    
    * Style fixes

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -4,9 +4,6 @@ import Header from './Header';
 import Footer from './Footer';
 
 import getDummyPhoto from '../logic/getPhoto';
-import createDummyPhoto from '../logic/createPhoto';
-
-import { PHOTO_TYPE } from '../constants/string';
 
 import '../style/PhotoList.css';
 
@@ -18,16 +15,14 @@ export default class PhotoList extends Component {
     };
   }
   componentWillMount(){
-    createDummyPhoto();
     let dummy = [];
-    dummy.unshift(getDummyPhoto('dummyPhoto1'));
-    dummy.unshift(getDummyPhoto('dummyPhoto2'));
-    dummy.unshift(getDummyPhoto('dummyPhoto3'));
+    for(let i = 1; i < 11; i++){
+      dummy.unshift(getDummyPhoto(`dummyPhoto${i}`));
+    }
     this.setState({photoList: dummy});
   }
   renderPhotoList = () => {
     const node = this.state.photoList.map((photo, i) => {
-      console.log(photo);      
       return (
         <div key={`photo-${i}`} className='photo-wrapper'>        
           <div className='photo-avatar'>{photo.photo_member[0]}</div>
@@ -37,12 +32,11 @@ export default class PhotoList extends Component {
             <h3>{photo.photo_costume}</h3>
             </div>
             <div className='photo-data-type'>
-              <h3>{PHOTO_TYPE[photo.photo_type]}</h3>
+              <h3>{photo.photo_type}</h3>
             </div>
             <div className='photo-data-number'>
               <h3>{photo.photo_number}</h3>
-            </div>
-            
+            </div>          
           </div>
         </div>
       )
@@ -50,8 +44,7 @@ export default class PhotoList extends Component {
     return node;
   }
   
-  render() {
-    
+  render() {    
     return (
       <div className='photo-list-view'>
         <Header />
