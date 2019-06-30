6131854a1110de9152c66463bf56053e827fbb7e
Asuka Wang <asukachikaru@gmail.com>
2019-06-30 20:12:23 +0900

    Style fixes, create button (#8)

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
 
+import '../style/Create.css';
+
 export default class Create extends Component {
   render() {
     return (
       <div className='create'>
-        <div className='meta-data'>
-          <div className='data-type-wrapper'>
-            <h3></h3>
-          </div>
-        </div>
+        +
       </div>
     )
   }
