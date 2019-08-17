b5e5bdb0a35acf5dba5ae441918cc881cb606c1d
Asuka Wang <asukachikaru@gmail.com>
2019-08-17 07:17:58 -0700

    Add full member data (#39)
    * Add full keyakizaka members data
    
    * Fix too much select-button overflow under main-button

--- src/constants/member.js
+++ src/constants/member.js
@@ -1,60 +1,214 @@
 export const members = {
   keyakizaka: [
     {
-      member_name_en: 'imaizumi_yui',
-      member_name: '今泉佑唯',
-      member_name_gana: 'いまいずみ_ゆい',
+      member_name_en: "ishimori_nijika",
+      member_name: "石森虹花",
+      member_name_gana: "いしもり_にじか",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "imaizumi_yui",
+      member_name: "今泉佑唯",
+      member_name_gana: "いまいずみ_ゆい",
       member_gen: 1,
       member_graduated: true
     },
     {
-      member_name_en: 'shida_manaka',
-      member_name: '志田愛佳',
-      member_name_gana: 'しだ_まなか',
+      member_name_en: "uemura_rina",
+      member_name: "上村莉菜",
+      member_name_gana: "うえむら_りな",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "ozeki_rika",
+      member_name: "尾関梨香",
+      member_name_gana: "おぜき_りか",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "oda_nana",
+      member_name: "織田奈那",
+      member_name_gana: "おだ_なな",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "koike_minami",
+      member_name: "小池美波",
+      member_name_gana: "こいけ_みなみ",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "kobayashi_yui",
+      member_name: "小林由依",
+      member_name_gana: "こばやし_ゆい",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "saito_fuyuka",
+      member_name: "齋藤冬優花",
+      member_name_gana: "さいとう_ふゆか",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "sato_shiori",
+      member_name: "佐藤詩織",
+      member_name_gana: "さとう_しおり",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "shida_manaka",
+      member_name: "志田愛佳",
+      member_name_gana: "しだ_まなか",
       member_gen: 1,
       member_graduated: true
     },
     {
-      member_name_en: 'sugai_yuuka',
-      member_name: '菅井友香',
-      member_name_gana: 'すがい_ゆうか',
+      member_name_en: "sugai_yuuka",
+      member_name: "菅井友香",
+      member_name_gana: "すがい_ゆうか",
       member_gen: 1,
       member_graduated: false
     },
     {
-      member_name_en: 'hirate_yurina',
-      member_name: '平手友梨奈',
-      member_name_gana: 'ひらて_ゆりな',
+      member_name_en: "suzumoto_miyu",
+      member_name: "鈴本美愉",
+      member_name_gana: "すずもと_みゆ",
       member_gen: 1,
       member_graduated: false
     },
     {
-      member_name_en: 'moriya_akane',
-      member_name: '守屋茜',
-      member_name_gana: 'もりや_あかね',
+      member_name_en: "nagasawa_nanako",
+      member_name: "長沢菜々香",
+      member_name_gana: "ながさわ_ななこ",
       member_gen: 1,
       member_graduated: false
     },
     {
-      member_name_en: 'inoue_rina',
-      member_name: '井上梨名',
-      member_name_gana: 'いのうえ_りな',
+      member_name_en: "nagahama_neru",
+      member_name: "長濱ねる",
+      member_name_gana: "ながはま_ねる",
+      member_gen: 1,
+      member_graduated: true
+    },
+    {
+      member_name_en: "habu_mizuho",
+      member_name: "土生瑞穂",
+      member_name_gana: "はぶ_みづほ",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "harada_aoi",
+      member_name: "原田葵",
+      member_name_gana: "はらだ_あおい",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "hirate_yurina",
+      member_name: "平手友梨奈",
+      member_name_gana: "ひらて_ゆりな",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "moriya_akane",
+      member_name: "守屋茜",
+      member_name_gana: "もりや_あかね",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "yonetani_nanami",
+      member_name: "米谷奈々未",
+      member_name_gana: "よねたに_ななみ",
+      member_gen: 1,
+      member_graduated: true
+    },
+    {
+      member_name_en: "watanabe_rika",
+      member_name: "渡辺梨加",
+      member_name_gana: "わたなべ_りか",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "watanabe_risa",
+      member_name: "渡邉理佐",
+      member_name_gana: "わたなべ_りさ",
+      member_gen: 1,
+      member_graduated: false
+    },
+    {
+      member_name_en: "inoue_rina",
+      member_name: "井上梨名",
+      member_name_gana: "いのうえ_りな",
       member_gen: 2,
       member_graduated: false
     },
     {
-      member_name_en: 'seki_yumiko',
-      member_name: '関有美子',
-      member_name_gana: 'せき_ゆみこ',
+      member_name_en: "seki_yumiko",
+      member_name: "関有美子",
+      member_name_gana: "せき_ゆみこ",
       member_gen: 2,
       member_graduated: false
     },
     {
-      member_name_en: 'yamasaki_ten',
-      member_name: '山﨑天',
-      member_name_gana: 'やまさき_てん',
+      member_name_en: "takemoto_yui",
+      member_name: "武元唯衣",
+      member_name_gana: "たけもと_ゆい",
       member_gen: 2,
       member_graduated: false
     },
-  ],
+    {
+      member_name_en: "tamura_hono",
+      member_name: "田村保乃",
+      member_name_gana: "たむら_ほの",
+      member_gen: 2,
+      member_graduated: false
+    },
+    {
+      member_name_en: "fujiyoshi_karin",
+      member_name: "藤吉夏鈴",
+      member_name_gana: "ふじよし_かりん",
+      member_gen: 2,
+      member_graduated: false
+    },
+    {
+      member_name_en: "matsuda_rina",
+      member_name: "松田里奈",
+      member_name_gana: "まつだ_りな",
+      member_gen: 2,
+      member_graduated: false
+    },
+    {
+      member_name_en: "matsudaira_riko",
+      member_name: "松平璃子",
+      member_name_gana: "まつだいら_りこ",
+      member_gen: 2,
+      member_graduated: false
+    },
+    {
+      member_name_en: "morita_hikaru",
+      member_name: "森田ひかる",
+      member_name_gana: "もりた_ひかる",
+      member_gen: 2,
+      member_graduated: false
+    },
+    {
+      member_name_en: "yamasaki_ten",
+      member_name: "山﨑天",
+      member_name_gana: "やまさき_てん",
+      member_gen: 2,
+      member_graduated: false
+    }
+  ]
 };
--- src/constants/string.js
+++
@@ -1,9 +0,0 @@
-export const PHOTO_TYPE = {
-  yori: "ヨリ",
-  hiki: "ヒキ",
-  chu: "チュウ"
-}
-
-export const MEMBER_NAME = {
-  // "higuchi hina"
-}
