af62d39ed53254db89f1dd87ca6837e657b5015c
asukachikaru <asukachikaru@gmail.com>
2019-08-25 15:59:14 +0900

    Fix #45

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import PhotoItem from './PhotoItem';
+import PhotoItem from "./PhotoItem";
 
-import { getPhotos } from '../services/actions';
-import { STORE_TYPES } from '../services/types';
-import '../style/PhotoList.css';
+import { getPhotos } from "../services/actions";
+import { STORE_TYPES } from "../services/types";
+import "../style/PhotoList.css";
 
 const mapDispatchToProps = {
   getPhotos
@@ -13,23 +13,24 @@ const mapDispatchToProps = {
 
 const mapStateToProps = state => {
   return {
-    [STORE_TYPES.STATE.TOP.PHOTOS]: state[STORE_TYPES.STATE.TOP.META][STORE_TYPES.STATE.TOP.PHOTOS]
-  }
+    [STORE_TYPES.STATE.TOP.PHOTOS]:
+      state[STORE_TYPES.STATE.TOP.META][STORE_TYPES.STATE.TOP.PHOTOS]
+  };
 };
 
 class PhotoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showActionSheet: false,
+      showActionSheet: false
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.getPhotos();
   }
 
-  countNum = photoInts => {
+  countSameClassPhotoNum = photoInts => {
     const thisInt = photoInts.pop();
     const photoItem = {
       photoMember: thisInt.photo_member,
@@ -37,46 +38,41 @@ class PhotoList extends Component {
       photoType: thisInt.photo_type,
       photoNumber: 1
     };
-    photoInts.forEach((photoInt, i) => {
-      if(
-        photoInt.photo_member === photoItem.photoMember &&
-        photoInt.photo_costume === photoItem.photoCostume &&
-        photoInt.photo_type === photoItem.photoType
+    const restPhotoInts = [];
+    photoInts.forEach(photoInt => {
+      if (
+        photoInt.photo_member !== photoItem.photoMember ||
+        photoInt.photo_costume !== photoItem.photoCostume ||
+        photoInt.photo_type !== photoItem.photoType
       ) {
-        photoInts.splice(i, 1);
-        photoItem.photoNumber++
-      }        
-    });        
-    return photoInts.length !== 0 
-      ? [...this.countNum(photoInts), photoItem]
-      : [photoItem]
-    ;
-  }
+        restPhotoInts.push(photoInt);
+      } else {
+        photoItem.photoNumber++;
+      }
+    });
+    return restPhotoInts.length !== 0
+      ? [...this.countSameClassPhotoNum(restPhotoInts), photoItem]
+      : [photoItem];
+  };
 
   renderPhotoList = () => {
     const photoInts = [...this.props.photos];
     let photoItems = [];
-    if(photoInts.length!==0) {
-      photoItems = this.countNum(photoInts);
+    if (photoInts.length !== 0) {
+      photoItems = this.countSameClassPhotoNum(photoInts);
     }
     const nodes = photoItems.map((photoItem, i) => {
-      return (
-        <PhotoItem
-          photo={photoItem}
-          key={`photo-item-${i}`}
-        />
-      )
+      return <PhotoItem photo={photoItem} key={`photo-item-${i}`} />;
     });
     return nodes;
   };
-  
+
   render() {
-    return (
-        <div className='photo-list-wrapper'>
-          {this.renderPhotoList()}
-        </div>
-    );
+    return <div className="photo-list-wrapper">{this.renderPhotoList()}</div>;
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(PhotoList);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PhotoList);
