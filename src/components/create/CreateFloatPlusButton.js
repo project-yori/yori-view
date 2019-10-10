db83894ec7670e631f70045d212cd306f899f9ec
asukachikaru <asukachikaru@gmail.com>
2019-10-10 22:33:37 +0900

    Hide CreateFloatPlusButton in search mode The implementation is to add a hide css class when `STATE.TOP.KEWORD_SEARCH` is not empty string.

--- src/components/create/CreateFloatPlusButton.js
+++ src/components/create/CreateFloatPlusButton.js
@@ -1,17 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { Add } from "@material-ui/icons";
+import { STORE_TYPES } from "../../services/types";
 
 import "../../style/create/CreateButton.css";
 
-export default class CreateButton extends Component {
-  render() {
-    return (
-      <div className="create">
-        <Link to="/create/group">
-          <Add>{this.props.children}</Add>
-        </Link>
-      </div>
-    );
-  }
-}
+const mapStateToProps = state => {
+  return {
+    [STORE_TYPES.STATE.TOP.KEYWORD_SEARCH]:
+      state[STORE_TYPES.STATE.TOP.META][STORE_TYPES.STATE.TOP.KEYWORD_SEARCH]
+  };
+};
+
+const CreateButton = ({ keywordSearch }) => {
+  return (
+    <div className={keywordSearch === "" ? "create" : "create hide"}>
+      <Link to="/create/group">
+        <Add></Add>
+      </Link>
+    </div>
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(CreateButton);
