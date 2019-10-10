86fc08f664f6a2ee48553713f89ec87d7cd24366
asukachikaru <asukachikaru@gmail.com>
2019-10-10 23:36:23 +0900

    Change Footer to functional compoennt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { changeSortType } from "../services/actions";
-import { SORT_TYPES } from "../services/types";
 import { Face, CameraFront, AccessTime } from "@material-ui/icons";
 
 import "../style/Footer.css";
-import { sort } from "../services/apis/sort";
 
 const mapStateToProps = state => {
   const { sortType } = state.top;
@@ -18,46 +16,41 @@ const mapDispatchToProps = {
   changeSortType
 };
 
-class Footer extends Component {
-  handleClickSort = sortType => {
+const Footer = ({ sortType, changeSortType }) => {
+  const handleClickSort = clickedSortType => {
     const newSortType =
-      sortType === this.props.sortType ? sortType + "Reverse" : sortType;
-    this.props.changeSortType(newSortType);
+      clickedSortType === sortType
+        ? clickedSortType + "Reverse"
+        : clickedSortType;
+    changeSortType(newSortType);
   };
-  render() {
-    return (
-      <footer>
-        <div className="button-wrapper">
-          <button
-            className="member"
-            onClick={() => this.handleClickSort("member")}
-          >
-            <Face />
-            メンバー順
-          </button>
-        </div>
-        <div className="button-wrapper">
-          <button
-            className="costume"
-            onClick={() => this.handleClickSort("costume")}
-          >
-            <CameraFront />
-            テーマ順
-          </button>
-        </div>
-        <div className="button-wrapper">
-          <button
-            className="createTime"
-            onClick={() => this.handleClickSort("createTime")}
-          >
-            <AccessTime />
-            登録順
-          </button>
-        </div>
-      </footer>
-    );
-  }
-}
+
+  return (
+    <footer>
+      <div className="button-wrapper">
+        <button className="member" onClick={() => handleClickSort("member")}>
+          <Face />
+          メンバー順
+        </button>
+      </div>
+      <div className="button-wrapper">
+        <button className="costume" onClick={() => handleClickSort("costume")}>
+          <CameraFront />
+          テーマ順
+        </button>
+      </div>
+      <div className="button-wrapper">
+        <button
+          className="createTime"
+          onClick={() => handleClickSort("createTime")}
+        >
+          <AccessTime />
+          登録順
+        </button>
+      </div>
+    </footer>
+  );
+};
 
 export default connect(
   mapStateToProps,
