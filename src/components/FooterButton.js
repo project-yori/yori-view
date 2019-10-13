340fd44d048b073f8c7881283aa1f27d3980a41f
asukachikaru <asukachikaru@gmail.com>
2019-10-13 18:15:35 +0900

    Add sort order indicator icon

--- src/components/FooterButton.js
+++ src/components/FooterButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { changeSortType } from "../services/actions";
+import { KeyboardArrowUp, KeyboardArrowDown } from "@material-ui/icons";
 
 const mapStateToProps = state => {
   const { sortType } = state.top;
@@ -19,6 +20,16 @@ const FooterButton = ({ children, sortType, changeSortType, thisSortType }) => {
       thisSortType === sortType ? thisSortType + "Reverse" : thisSortType;
     changeSortType(newSortType);
   };
+  const sortTypeIcon = children[0];
+  const sortTypeTxt = children[1];
+  const sortOrderIcon =
+    RegExp(thisSortType).exec(sortType) === null ? null : /Reverse/.exec(
+        sortType
+      ) !== null ? (
+      <KeyboardArrowDown />
+    ) : (
+      <KeyboardArrowUp />
+    );
   return (
     <div className="button-wrapper">
       <button
@@ -29,7 +40,11 @@ const FooterButton = ({ children, sortType, changeSortType, thisSortType }) => {
         }
         onClick={() => handleClickSort(thisSortType)}
       >
-        {children}
+        {sortTypeIcon}
+        <div className="button-text-container">
+          {sortTypeTxt}
+          {sortOrderIcon}
+        </div>
       </button>
     </div>
   );
